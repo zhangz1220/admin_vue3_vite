784b1ed1ad9851e097fbd80ac6120252189b1806
zhangz1220 <13309635539@163.com>
2024-05-01 16:30:32 +0800

    放仓库
    放仓库

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<router-view />
+</template>
+
+<style></style>
+
+<script>
+
+export default {
+	name: "app"
+};
+</script>
---
+++ src/layout/components/Sidebar/MenuItem.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<template v-if="icon">
+			<svg-icon :icon="icon"></svg-icon>
+		</template>
+
+		<span :style="icon?'':'margin-left: 5px'">{{title}}</span>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+  .sub-el-icon{
+    width: 1em;
+    height: 1em;
+    color: currentcolor;
+
+  }
+
+</style>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+	title: {
+		type: String,
+		required: true
+	},
+	icon: {
+		type: String,
+		required: true
+	}
+});
+</script>
